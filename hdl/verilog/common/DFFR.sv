d99a3091865810691e6048c85da904743f4ba1d0
Tianyu Li <Tianyu.Li@verisilicon.com>
2024-11-19 14:34:25 +0800

    Add common DFFT and EDFFR.
    Change-Id: I6a24bfa91f3e3500d04fce3c075516c6db3e8a78

---
+++ hdl/verilog/common/DFFR.sv
@@ -0,0 +1,16 @@
+//   DFFR   - posedge ff w/ rst_n,
+//   Order of ports is:  q, clk, [rst_n], [e], d
+//   E.g.  DFF #(4) qsig (qsig, clk, rst_n, dsig);
+
+module DFFR ( q, clk, rst_n, d ) ; // FF with async rst_n;  
+  parameter WIDTH = 1 ;
+  input 	clk ;
+  input 	rst_n ;
+  input  [WIDTH-1:0] d ;
+  output [WIDTH-1:0] q ;
+  reg [WIDTH-1:0] 	 q ;
+  
+  always @(posedge clk or negedge rst_n) 
+    if (!rst_n) q <= {WIDTH{1'b0}} ;
+    else q <= d ;
+endmodule
