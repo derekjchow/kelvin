4a8156241e3a570845c392e1740fbcc530cd55c3
Zhidong Liang <Zhidong.Liang@verisilicon.com>
2024-11-19 14:33:58 +0800

    add rvv_vrf_reg module and edff common module.
    Change-Id: I43179fc5e827e274a31e3888071de8b8a325dbf2

---
+++ hdl/verilog/common/edff.sv
@@ -0,0 +1,18 @@
+// edff - posedge ff, async rst_n, sync enable.
+
+module edff (q, en, d, clk, rst_n);
+  parameter WIDTH = 1;
+  parameter INIT  = '0;
+
+  output logic [WIDTH-1:0] q;
+
+  input  logic             en;
+  input  logic [WIDTH-1:0] d;
+  input  logic             clk;
+  input  logic             rst_n;
+
+  always @(posedge clk or negedge rst_n)
+    if (!rst_n)   q <= INIT;
+    else if (en)  q <= d;
+
+endmodule
