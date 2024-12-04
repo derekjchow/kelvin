e0053312ab55788237bde8670834c2018aa03410
Derek Chow <derekjchow@google.com>
2024-12-04 14:27:26 -0800

    Add RvvFrontEnd.
    Change-Id: Ife120a5006b931bf1c7f49f25a8d4a5881097927

--- hdl/verilog/rvv/design/rvv.svh
+++ hdl/verilog/rvv/design/rvv.svh
@@ -1,7 +1,65 @@
 `ifndef HDL_VERILOG_RVV_DESIGN_RVV_SVH
 `define HDL_VERILOG_RVV_DESIGN_RVV_SVH
 
+`ifndef HDL_VERILOG_RVV_DESIGN_RVV_DEFINE_SVH
 `include "rvv_define.svh"
+`endif  // not defined HDL_VERILOG_RVV_DESIGN_RVV_DEFINE_SVH
+
+// Enum type for SEW. See Table 2 in:
+// https://github.com/riscv/riscv-v-spec/blob/master/v-spec.adoc#341-vector-selected-element-width-vsew20
+typedef enum logic [2:0] {
+  SEW8=0,
+  SEW16=1,
+  SEW32=2,
+  SEW64=3
+} RVVSEW;
+
+// Enum type for LMUL. See:
+// https://github.com/riscv/riscv-v-spec/blob/master/v-spec.adoc#vector-instruction-formats
+typedef enum logic [2:0] {
+  LMUL1=0,
+  LMUL2=1,
+  LMUL4=2,
+  LMUL8=3,
+  LMULRESERVED=4,
+  LMUL1_8=5, // 1/8
+  LMUL1_4=6, // 1/4
+  LMUL1_2=7  // 1/2
+} RVVLMUL;
+
+// The architectural configuration state of the RVV core.
+typedef struct packed {
+  logic [7:0] vl;  // Max 128, need one extra bit
+  logic ma;
+  logic ta;
+  RVVSEW sew;
+  RVVLMUL lmul;
+} RVVConfigState;
+
+// Enum to encode the major opcode of the instruction. See "Section 5. Vector
+// Instruction Formats" of the RVV 1.0 spec.
+typedef enum logic [1:0] {
+  LOAD=0,
+  STORE=1,
+  RVV=2
+} RVVOpCode;
+
+// A decoded instruction forwarded to the RVVCore from the scalar core.
+typedef struct packed {
+  logic [31:0] pc;
+  RVVOpCode opcode; // effectively bits [6:0] from instruction
+  logic [24:0] bits;   // bits [31:7] from instruction
+} RVVInstruction;
+
+// A command internal to the RVVCore. The immediate value of this command has
+// been read from the scalar register file if necessary. It also contains
+// additional data to track configuration register state (ie: SEW, LMUL, etc).
+typedef struct packed {
+  RVVOpCode opcode;
+  logic [24:0] bits;
+  logic [31:0] rs1;
+  RVVConfigState arch_state;
+} RVVCmd;
 
 //
 // IF stage, RVS to Command Queue
@@ -28,7 +86,8 @@ typedef struct packed {
 
 typedef struct packed {
     logic   [`PC_WIDTH-1:0]         insts_pc;
-    logic   [`INST_WIDTH-1:0]       insts;
+    RVVOpCode opcode;
+    logic [24:0] bits;
     VECTOR_CSR_t                    vector_csr;
     logic   [`XLEN-1:0] 	        rs1_data;
 } INST_t;
