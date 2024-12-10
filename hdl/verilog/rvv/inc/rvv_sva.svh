2bc6c12b16f8ab7ab5a8b057dc164b2bff7d90b5
Mingzhe Chen <Mingzhe.Chen@verisilicon.com>
2024-12-10 11:11:35 +0800

    Remove svh files and try to use p4 copy to keep revision history
    Change-Id: Idee7d88fd3720f403c578c5e0ae3aa6d57f25120

--- hdl/verilog/rvv/inc/rvv_sva.svh
+++
@@ -1,6 +0,0 @@
-// SV2009 feature - default values for arguments
-`define rvv_expect(prop) assert property (@(posedge clk) disable iff (~rst_n) prop)
-`define rvv_forbid(seq)  assert property (@(posedge clk) disable iff (~rst_n) not (strong(seq)))
-`define rvv_cover(seq)   cover  property (@(posedge clk) disable iff (~rst_n) seq)
-`define rvv_assume(prop) assume property (@(posedge clk) disable iff (~rst_n) prop)
-
