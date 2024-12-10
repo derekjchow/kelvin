f49d7ad1e5f12a25765852694d3e90af099ce199
Tianyu Li <Tianyu Li Tianyu.Li@verisilicon.com>
2024-12-10 11:10:45 +0800

    add UQ_WIDTH
    Change-Id: I4727390adc91f39ac63601e424a5b9c2d03aedfc

--- hdl/verilog/rvv/design/rvv_define.svh
+++ hdl/verilog/rvv/design/rvv_define.svh
@@ -9,6 +9,7 @@
 
 // the max number of uops are written to Uops Queue per cycle in DE stage
 `define NUM_DE_UOP              4
+`define NUM_DE_UOP_WIDTH        $clog2(`NUM_DE_UOP)+1
 
 // the max number of uops are dispated per cycle in DP stage
 `define NUM_DP_UOP              2
@@ -16,8 +17,8 @@
 // the max number of uops are retired per cycle in WB stage
 `define NUM_RT_UOP              4
 
-`define CQ_DEPTH
-`define UQ_DEPTH
+`define CQ_DEPTH                8
+`define UQ_DEPTH                16
 `define ROB_DEPTH               8
 `define ROB_DEPTH_WIDTH         $clog2(`ROB_DEPTH)
 
@@ -81,6 +82,9 @@
 `define OPCODE_STORE            2'b01
 // configuration(vset*) and arithmetic instructions
 `define OPCODE_ARI_CFG          2'b10
+
+// Uops Queue data width
+`define UQ_WIDTH                $bits(UOP_QUEUE_t)
 
 // Reservation Station data width
 `define ALU_RS_WIDTH            $bits(ALU_RS_t)
