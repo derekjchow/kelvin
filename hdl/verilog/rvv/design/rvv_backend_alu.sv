0ef50df8a27d5d0da514141b4b6ceab7a3d0ba0a
Tianyu Li <Tianyu.Li@verisilicon.com>
2024-12-10 11:17:25 +0800

    modify to struct signals
    Change-Id: Ife4763f5616b369506f627c5d4a41819472d7e04

--- hdl/verilog/rvv/design/rvv_backend_alu.sv
+++ hdl/verilog/rvv/design/rvv_backend_alu.sv
@@ -6,88 +6,72 @@
 
 `include "rvv_backend.svh"
 
-module _alu
+module rvv_backend_alu
 (
-  pop0_ex2rs,
-  pop1_ex2rs,
-  alu_uop0_rs2ex,
-  alu_uop1_rs2ex,
+  pop_ex2rs,
+  alu_uop_rs2ex,
   fifo_empty_rs2ex,
   fifo_1left_to_empty_rs2ex,
   
-  result0_valid_ex2rob,
-  result0_ex2rob,
-  result0_ready_rob2alu,
-  result1_valid_ex2rob,
-  result1_ex2rob,
-  result1_ready_rob2alu
+  result_valid_ex2rob,
+  result_ex2rob,
+  result_ready_rob2alu,
 );
 
 //
 // interface signals
 //
   // ALU RS to ALU unit
-  output  logic             pop0_ex2rs;
-  output  logic             pop1_ex2rs;
-  input   ALU_RS_t          alu_uop0_rs2ex;
-  input   ALU_RS_t          alu_uop1_rs2ex;
-  input   logic             fifo_empty_rs2ex;
-  input   logic             fifo_1left_to_empty_rs2ex;
+  output  logic       [`NUM_ALU_UOP-1:0]    pop_ex2rs;
+  input   ALU_RS_t    [`NUM_ALU_UOP-1:0]    alu_uop_rs2ex;
+  input   logic                             fifo_empty_rs2ex;
+  input   logic                             fifo_1left_to_empty_rs2ex;
 
   // submit ALU result to ROB
-  output  logic             result0_valid_ex2rob;
-  output  ALU2ROB_t         result0_ex2rob;
-  input   logic             result0_ready_rob2alu;
-  output  logic             result1_valid_ex2rob;
-  output  ALU2ROB_t         result1_ex2rob;
-  input   logic             result1_ready_rob2alu;
+  output  logic       [`NUM_ALU_UOP-1:0]    result_valid_ex2rob;
+  output  ALU2ROB_t   [`NUM_ALU_UOP-1:0]    result_ex2rob;
+  input   logic       [`NUM_ALU_UOP-1:0]    result_ready_rob2alu;
 
 //
 // internal signals
 //
   // ALU RS to ALU unit
-  logic                     alu_uop0_valid_rs2ex;                   
-  logic                     alu_uop1_valid_rs2ex;                   
+  logic               [`NUM_ALU_UOP-1:0]    alu_uop_valid_rs2ex;    
+
+  genvar                                    i;
 
 //
 // Instantiate 2 rvv_backend_alu_unit
 //
   // generate valid signals
-  assign  alu_uop0_valid_rs2ex = !fifo_empty_rs2ex;
-  assign  alu_uop1_valid_rs2ex = !(fifo_empty_rs2ex&fifo_1left_to_empty_rs2ex);
+  assign  alu_uop_valid_rs2ex[0] = !fifo_empty_rs2ex;
+  assign  alu_uop_valid_rs2ex[1] = !(fifo_empty_rs2ex&fifo_1left_to_empty_rs2ex);
   
   // generate pop signals
   // it can pop alu_uop1 when it can also pop alu_uop0. Otherwise, it cannot pop alu_uop1.(That's in-ordered issue from RS) 
-  assign  pop0_ex2rs = alu_uop0_valid_rs2ex&result0_valid_ex2rob&result0_ready_rob2alu;
-  assign  pop1_ex2rs = pop0&(alu_uop1_valid_rs2ex&result1_valid_ex2rob&result1_ready_rob2alu);  
+  assign  pop_ex2rs[0] = alu_uop_valid_rs2ex[0]&result_valid_ex2rob[1]&result_ready_rob2alu[0];
+  assign  pop_ex2rs[1] = pop_ex2rs[0]&(alu_uop_valid_rs2ex[1]&result_valid_ex2rob[1]&result_ready_rob2alu[1]);  
 
   // instantiate
-  rvv_backend_alu_unit u_alu_unit0
-  (
-    // inputs
-    .alu_uop_valid          (alu_uop0_valid_rs2ex),
-    .alu_uop                (alu_uop0_rs2ex),
-    // outputs
-    .result_ex2rob_valid    (result0_valid_ex2rob),
-    .result_ex2rob          (result0_ex2rob)
-  );
-
-  rvv_backend_alu_unit u_alu_unit1
-  (
-    // inputs
-    .alu_uop_valid          (alu_uop1_valid_rs2ex),
-    .alu_uop                (alu_uop1_rs2ex),
-    // outputs
-    .result_valid_ex2rob    (result1_valid_ex2rob),
-    .result_ex2rob          (result1_ex2rob)
-  );
-
+  for (i=0;i<`NUM_ALU_UOP;i=i+1) 
+  begin: ALU_UNIT
+  rvv_backend_alu_unit u_alu_unit[i]
+    (
+      // inputs
+      .alu_uop_valid          (alu_uop_valid_rs2ex[i]),
+      .alu_uop                (alu_uop_rs2ex[i]),
+      // outputs
+      .result_ex2rob_valid    (result_valid_ex2rob[i]),
+      .result_ex2rob          (result_ex2rob[i])
+    );
+  end
 
 `ifdef ASSERT_ON
-  `rvv_forbid(alu_uop0_valid_rs2ex&(!result0_valid_ex2rob)) 
-    else $error("rob_entry=%d. Something wrong in alu_unit0 decoding and execution.\n",alu_uop0_rs2ex.rob_entry);
+  `rvv_forbid(alu_uop_valid_rs2ex[0]&(!result_valid_ex2rob[0])) 
+    else $error("rob_entry=%d. Something wrong in alu_unit0 decoding and execution.\n",alu_uop_rs2ex[0].rob_entry);
 
-  `rvv_forbid(alu_uop1_valid_rs2ex&(!result1_valid_ex2rob)) 
-    else $error("rob_entry=%d. Something wrong in alu_unit1 decoding and execution.\n",alu_uop1_rs2ex.rob_entry);
+  `rvv_forbid(alu_uop_valid_rs2ex[1]&(!result_valid_ex2rob[1])) 
+    else $error("rob_entry=%d. Something wrong in alu_unit1 decoding and execution.\n",alu_uop1_rs2ex[1].rob_entry);
 `endif
+
 endmodule
