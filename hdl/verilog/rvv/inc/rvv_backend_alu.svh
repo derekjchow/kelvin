639aeeaab7f21ab3b579d024e047ad1c9e4c36ef
Tianyu Li <Tianyu.Li@verisilicon.com>
2024-12-13 14:11:23 +0800

    Add F_SHIFT_t for shift instructions
    Change-Id: I8895c71820d37df3d5800f4ddefa863f136912e8

--- hdl/verilog/rvv/inc/rvv_backend_alu.svh
+++ hdl/verilog/rvv/inc/rvv_backend_alu.svh
@@ -1,5 +1,11 @@
-
 typedef enum logic [0:0]{
   ADDSUB_VADD, 
   ADDSUB_VSUB
-} F_ADDSUB_t;               
+} F_ADDSUB_t;   
+
+typedef enum logic [1:0]{
+  SHIFT_SLL, 
+  SHIFT_SRL,
+  SHIFT_SRA
+} F_SHIFT_t;   
+
