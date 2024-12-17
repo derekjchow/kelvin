41a5113f56cc2e531d13f44256208697a7c1f37b
Tianyu Li <Tianyu.Li@verisilicon.com>
2024-12-17 15:26:38 +0800

    Add GET_MIN_MAX_e for vmin and vmax instructions
    Change-Id: Iebb36e9e72a05db5448846125c5ecd4c7e300a07

--- hdl/verilog/rvv/inc/rvv_backend_alu.svh
+++ hdl/verilog/rvv/inc/rvv_backend_alu.svh
@@ -4,13 +4,17 @@
 typedef enum logic [0:0]{
   ADDSUB_VADD, 
   ADDSUB_VSUB
-} F_ADDSUB_e;   
+} ADDSUB_e;   
 
 typedef enum logic [1:0]{
   SHIFT_SLL, 
   SHIFT_SRL,
   SHIFT_SRA
-} F_SHIFT_e;   
+} SHIFT_e;   
+
+typedef enum logic [0:0]{
+  GET_MIN, 
+  GET_MAX
+} GET_MIN_MAX_e;   
 
 `endif // ALU_DEFINE_SVH
-
