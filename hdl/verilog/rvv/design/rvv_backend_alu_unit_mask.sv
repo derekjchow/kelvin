8ecef2cb043878be890448108e9885e7287bcefe
Tianyu Li <Tianyu.Li@verisilicon.com>
2024-12-20 13:35:42 +0800

    complete viota and vid instructions
    Change-Id: I05236ee494093a34b0798dbd344d21390dfbfffc

--- hdl/verilog/rvv/design/rvv_backend_alu_unit_mask.sv
+++ hdl/verilog/rvv/design/rvv_backend_alu_unit_mask.sv
@@ -33,6 +33,7 @@ module rvv_backend_alu_unit_mask
   logic                               v0_data_valid;
   logic   [`VLEN-1:0]                 vd_data;           
   logic                               vd_data_valid;
+  EEW_e                               vd_eew;
   logic   [`REGFILE_INDEX_WIDTH-1:0]  vs1_opcode;              
   logic   [`VLEN-1:0]                 vs1_data;           
   logic                               vs1_data_valid; 
@@ -41,25 +42,27 @@ module rvv_backend_alu_unit_mask
   EEW_e                               vs2_eew;
   logic   [`XLEN-1:0] 	              rs1_data;        
   logic        	                      rs1_data_valid;
+  logic   [`UOP_INDEX_WIDTH-1:0]      uop_index;          
 
   // execute 
-  logic   [`VLEN-1:0]         src2_data;
-  logic   [`VLEN-1:0]         src1_data;
-  logic   [`VLEN-1:0]         tail_data;
-  logic   [`VLEN-1:0]         result_data;
-  logic   [`VLEN-1:0]         result_data_andn;
-  logic   [`VLEN-1:0]         result_data_and; 
-  logic   [`VLEN-1:0]         result_data_or;  
-  logic   [`VLEN-1:0]         result_data_xor; 
-  logic   [`VLEN-1:0]         result_data_orn; 
-  logic   [`VLEN-1:0]         result_data_nand;
-  logic   [`VLEN-1:0]         result_data_nor; 
-  logic   [`VLEN-1:0]         result_data_xnor;
-  logic   [`VLEN-1:0]         result_data_vmsof;
-  logic   [`VLEN-1:0]         result_data_vmsif;
-  logic   [`VLEN-1:0]         result_data_vmsbf;
-  logic   [`VLEN-1:0]         result_data_vfirst;
-  logic   [`VLEN/16-1:0][4:0] result_data_vcpop;
+  logic   [`VLEN-1:0]                     src2_data;
+  logic   [`VLEN-1:0]                     src1_data;
+  logic   [`VLEN-1:0]                     tail_data;
+  logic   [`VLEN-1:0]                     result_data;
+  logic   [`VLEN-1:0]                     result_data_andn;
+  logic   [`VLEN-1:0]                     result_data_and; 
+  logic   [`VLEN-1:0]                     result_data_or;  
+  logic   [`VLEN-1:0]                     result_data_xor; 
+  logic   [`VLEN-1:0]                     result_data_orn; 
+  logic   [`VLEN-1:0]                     result_data_nand;
+  logic   [`VLEN-1:0]                     result_data_nor; 
+  logic   [`VLEN-1:0]                     result_data_xnor;
+  logic   [`VLEN-1:0]                     result_data_vmsof;
+  logic   [`VLEN-1:0]                     result_data_vmsif;
+  logic   [`VLEN-1:0]                     result_data_vmsbf;
+  logic   [`VLEN-1:0]                     result_data_vfirst;
+  logic   [`VLEN/16-1:0][4:0]             result_data_vcpop;
+  logic   [`VLEN-1:0][$clog2(`VLEN)-1:0]  result_data_viota;
 
   // PU2ROB_t  struct signals
   logic   [`VLEN-1:0]             w_data;             // when w_type=XRF, w_data[`XLEN-1:0] will store the scalar result
@@ -86,6 +89,7 @@ module rvv_backend_alu_unit_mask
   assign  v0_data_valid  = alu_uop.v0_data_valid;
   assign  vd_data        = alu_uop.vd_data;
   assign  vd_data_valid  = alu_uop.vd_data_valid;
+  assign  vd_eew         = alu_uop.vd_eew;
   assign  vs1_opcode     = alu_uop.vs1;
   assign  vs1_data       = alu_uop.vs1_data;
   assign  vs1_data_valid = alu_uop.vs1_data_valid;
@@ -94,6 +98,7 @@ module rvv_backend_alu_unit_mask
   assign  vs2_eew        = alu_uop.vs2_eew;
   assign  rs1_data       = alu_uop.rs1_data;
   assign  rs1_data_valid = alu_uop.rs1_data_valid;
+  assign  uop_index      = alu_uop.uop_index;
   
 //  
 // prepare source data 
@@ -251,6 +256,40 @@ module rvv_backend_alu_unit_mask
                     else $error("vd_data_valid(%d) should be 1 when vm=0.\n",vd_data_valid);
                 `endif
               end
+              VIOTA: begin
+                if((vs1_data_valid==1'b0)&vs2_data_valid&((vm==1'b1)||((vm==1'b0)&v0_data_valid))) begin
+                  result_valid = 1'b1;
+                  
+                  if (vm==1'b1)
+                    src2_data = vs2_data;
+                  else
+                    src2_data = vs2_data&v0_data; 
+                end 
+
+                `ifdef ASSERT_ON
+                  `rvv_expect(vs1_data_valid==1'b0) 
+                    else $error("vs1_data_valid(%d) should be 0.\n",vs1_data_valid);
+  
+                  `rvv_expect(vs2_data_valid==1'b1) 
+                    else $error("vs2_data_valid(%d) should be 1.\n",vs2_data_valid);
+                `endif
+              end
+              VID: begin
+                // vid is the same as viota when vs2 is {`VLEN{1'b1}}.
+                if((vs1_data_valid==1'b0)&((vm==1'b1)||((vm==1'b0)&v0_data_valid))) begin
+                  result_valid = 1'b1;
+                  
+                  if (vm==1'b1)
+                    src2_data = {`VLEN{1'b1}};
+                  else
+                    src2_data = v0_data; 
+                end 
+
+                `ifdef ASSERT_ON
+                  `rvv_expect(vs1_data_valid==1'b0) 
+                    else $error("vs1_data_valid(%d) should be 0.\n",vs1_data_valid);
+                `endif
+              end
             endcase
           end
         endcase
@@ -272,7 +311,7 @@ module rvv_backend_alu_unit_mask
   assign result_data_vmsof = (src2_data==0) ? {`VLEN{1'b1}} : (~(src2_data-1))&src2_data;  // find first 1 from LSB
   assign result_data_vmsif = (src2_data==0) ? {`VLEN{1'b1}} : (result_data_vmsof-1)^result_data_vmsof;  
   assign result_data_vmsbf = (src2_data==0) ? {`VLEN{1'b1}} : ({1'b0,result_data_vmsof[`VLEN-1:1]}-1)^{1'b0,result_data_vmsof[`VLEN-1:1]};  
-  
+ 
   // vfirst
   always_comb begin
     result_data_vfirst = 'b0;
@@ -294,6 +333,15 @@ module rvv_backend_alu_unit_mask
                                      ((src2_data[16*j+4] +src2_data[16*j+5] ) + (src2_data[16*j+6] +src2_data[16*j+7] ))) + 
                                     (((src2_data[16*j+8] +src2_data[16*j+9] ) + (src2_data[16*j+10]+src2_data[16*j+11])) +
                                      ((src2_data[16*j+12]+src2_data[16*j+13]) + (src2_data[16*j+14]+src2_data[16*j+15])));
+    end
+  endgenerate
+
+  // viota and vid
+  assign result_data_viota[0] = 'b0;
+
+  generate 
+    for(j=1;j<`VLEN;j++) begin: GET_VIOTA
+      assign result_data_viota[j] = src2_data[j-1] + result_data_viota[j-1]; 
     end
   endgenerate
 
@@ -369,6 +417,143 @@ module rvv_backend_alu_unit_mask
               VMSIF: begin
                 result_data = result_data_vmsif;
               end
+              VIOTA,
+              VID: begin
+                case(vd_eew)
+                  EEW8: begin
+                    case(uop_index)
+                      3'd0: begin
+                        for (i=0; i<`VLENB;i++) begin
+                          result_data[i*`BYTE_WIDTH +: `BYTE_WIDTH] = result_data_viota[i];
+                        end
+                      end
+                      3'd1: begin
+                        for (i=0; i<`VLENB;i++) begin
+                          result_data[i*`BYTE_WIDTH +: `BYTE_WIDTH] = result_data_viota[1*`VLENB+i];
+                        end
+                      end
+                      3'd2: begin
+                        for (i=0; i<`VLENB;i++) begin
+                          result_data[i*`BYTE_WIDTH +: `BYTE_WIDTH] = result_data_viota[2*`VLENB+i];
+                        end
+                      end
+                      3'd3: begin
+                        for (i=0; i<`VLENB;i++) begin
+                          result_data[i*`BYTE_WIDTH +: `BYTE_WIDTH] = result_data_viota[3*`VLENB+i];
+                        end
+                      end
+                      3'd4: begin
+                        for (i=0; i<`VLENB;i++) begin
+                          result_data[i*`BYTE_WIDTH +: `BYTE_WIDTH] = result_data_viota[4*`VLENB+i];
+                        end
+                      end
+                      3'd5: begin
+                        for (i=0; i<`VLENB;i++) begin
+                          result_data[i*`BYTE_WIDTH +: `BYTE_WIDTH] = result_data_viota[5*`VLENB+i];
+                        end
+                      end
+                      3'd6: begin
+                        for (i=0; i<`VLENB;i++) begin
+                          result_data[i*`BYTE_WIDTH +: `BYTE_WIDTH] = result_data_viota[6*`VLENB+i];
+                        end
+                      end
+                      3'd7: begin
+                        for (i=0; i<`VLENB;i++) begin
+                          result_data[i*`BYTE_WIDTH +: `BYTE_WIDTH] = result_data_viota[7*`VLENB+i];
+                        end
+                      end
+                    endcase
+                  end
+                  EEW16: begin
+                    case(uop_index)
+                      3'd0: begin
+                        for (i=0; i<`VLEN/`HWORD_WIDTH;i++) begin
+                          result_data[i*`HWORD_WIDTH +: `HWORD_WIDTH] = result_data_viota[i];
+                        end
+                      end
+                      3'd1: begin
+                        for (i=0; i<`VLEN/`HWORD_WIDTH;i++) begin
+                          result_data[i*`HWORD_WIDTH +: `HWORD_WIDTH] = result_data_viota[1*`VLEN/`HWORD_WIDTH+i];
+                        end
+                      end
+                      3'd2: begin
+                        for (i=0; i<`VLEN/`HWORD_WIDTH;i++) begin
+                          result_data[i*`HWORD_WIDTH +: `HWORD_WIDTH] = result_data_viota[2*`VLEN/`HWORD_WIDTH+i];
+                        end
+                      end
+                      3'd3: begin
+                        for (i=0; i<`VLEN/`HWORD_WIDTH;i++) begin
+                          result_data[i*`HWORD_WIDTH +: `HWORD_WIDTH] = result_data_viota[3*`VLEN/`HWORD_WIDTH+i];
+                        end
+                      end
+                      3'd4: begin
+                        for (i=0; i<`VLEN/`HWORD_WIDTH;i++) begin
+                          result_data[i*`HWORD_WIDTH +: `HWORD_WIDTH] = result_data_viota[4*`VLEN/`HWORD_WIDTH+i];
+                        end
+                      end
+                      3'd5: begin
+                        for (i=0; i<`VLEN/`HWORD_WIDTH;i++) begin
+                          result_data[i*`HWORD_WIDTH +: `HWORD_WIDTH] = result_data_viota[5*`VLEN/`HWORD_WIDTH+i];
+                        end
+                      end
+                      3'd6: begin
+                        for (i=0; i<`VLEN/`HWORD_WIDTH;i++) begin
+                          result_data[i*`HWORD_WIDTH +: `HWORD_WIDTH] = result_data_viota[6*`VLEN/`HWORD_WIDTH+i];
+                        end
+                      end
+                      3'd7: begin
+                        for (i=0; i<`VLEN/`HWORD_WIDTH;i++) begin
+                          result_data[i*`HWORD_WIDTH +: `HWORD_WIDTH] = result_data_viota[7*`VLEN/`HWORD_WIDTH+i];
+                        end
+                      end
+                    endcase
+                  end
+                  EEW32: begin
+                    case(uop_index)
+                      3'd0: begin
+                        for (i=0; i<`VLEN/`WORD_WIDTH;i++) begin
+                          result_data[i*`WORD_WIDTH +: `WORD_WIDTH] = result_data_viota[i];
+                        end
+                      end
+                      3'd1: begin
+                        for (i=0; i<`VLEN/`WORD_WIDTH;i++) begin
+                          result_data[i*`WORD_WIDTH +: `WORD_WIDTH] = result_data_viota[1*`VLEN/`WORD_WIDTH+i];
+                        end
+                      end
+                      3'd2: begin
+                        for (i=0; i<`VLEN/`WORD_WIDTH;i++) begin
+                          result_data[i*`WORD_WIDTH +: `WORD_WIDTH] = result_data_viota[2*`VLEN/`WORD_WIDTH+i];
+                        end
+                      end
+                      3'd3: begin
+                        for (i=0; i<`VLEN/`WORD_WIDTH;i++) begin
+                          result_data[i*`WORD_WIDTH +: `WORD_WIDTH] = result_data_viota[3*`VLEN/`WORD_WIDTH+i];
+                        end
+                      end
+                      3'd4: begin
+                        for (i=0; i<`VLEN/`WORD_WIDTH;i++) begin
+                          result_data[i*`WORD_WIDTH +: `WORD_WIDTH] = result_data_viota[4*`VLEN/`WORD_WIDTH+i];
+                        end
+                      end
+                      3'd5: begin
+                        for (i=0; i<`VLEN/`WORD_WIDTH;i++) begin
+                          result_data[i*`WORD_WIDTH +: `WORD_WIDTH] = result_data_viota[5*`VLEN/`WORD_WIDTH+i];
+                        end
+                      end
+                      3'd6: begin
+                        for (i=0; i<`VLEN/`WORD_WIDTH;i++) begin
+                          result_data[i*`WORD_WIDTH +: `WORD_WIDTH] = result_data_viota[6*`VLEN/`WORD_WIDTH+i];
+                        end
+                      end
+                      3'd7: begin
+                        for (i=0; i<`VLEN/`WORD_WIDTH;i++) begin
+                          result_data[i*`WORD_WIDTH +: `WORD_WIDTH] = result_data_viota[7*`VLEN/`WORD_WIDTH+i];
+                        end
+                      end
+                    endcase
+                  end
+                endcase
+              end
             endcase
           end
         endcase
@@ -439,6 +624,10 @@ module rvv_backend_alu_unit_mask
                       w_data[j] = result_data[j];
                     else 
                       w_data[j] = (tail_data[j]==1'b1)&(v0_data[j]==1'b0) ? vd_data[j] : result_data[j]; 
+                  end
+                  VIOTA,
+                  VID: begin
+                    w_data[j] = result_data[j];
                   end
                 endcase
               end
