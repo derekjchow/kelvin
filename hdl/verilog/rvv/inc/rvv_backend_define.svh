7398dae3ea64f2a60ca5c94e61e2ed1385fdc8fa
Tianyu Li <Tianyu.Li@verisilicon.com>
2025-01-13 16:11:58 -0800

    add BYTE_WIDTH HWORD_WIDTH WORD_WIDTH
    Change-Id: Ifdd9644310bd3ee7c011b2baf4624aceecd9b430

--- hdl/verilog/rvv/inc/rvv_backend_define.svh
+++ hdl/verilog/rvv/inc/rvv_backend_define.svh
@@ -32,9 +32,10 @@
 `define ROB_DEPTH_WIDTH         $clog2(`ROB_DEPTH)
 
 `define PC_WIDTH                32
-`define INST_WIDTH              27
 `define XLEN                    32
-`define REGFILE_INDEX_WIDTH     5
+`define BYTE_WIDTH              8
+`define HWORD_WIDTH             16
+`define WORD_WIDTH              32
 
 // an instruction will be split to EMUL_max=8 uops at most
 `define EMUL_MAX                8
@@ -43,8 +44,7 @@
 // Vector CSR
 `define VLEN                    128
 `define VLENB                   `VLEN/8
-
-// vstart < VLMAX_max and vl <= VLMAX_max, VLMAX_max=VLEN*LMUL_max/SEW_min=12
+// vstart < VLMAX_max and vl <= VLMAX_max, VLMAX_max=VLEN*LMUL_max/SEW_min=128
 `define VSTART_WIDTH            $clog2(`VLEN)
 `define VL_WIDTH                $clog2(`VLEN)+1
 `define VTYPE_VILL_WIDTH        1
@@ -59,14 +59,11 @@
 `define FUNCT6_WIDTH            6
 `define NFIELD_WIDTH            3
 `define VM_WIDTH                1
-`define VS2_WIDTH               5
+`define REGFILE_INDEX_WIDTH     5
 `define UMOP_WIDTH              5
-`define VS1_WIDTH               5
 `define NREG_WIDTH              3
 `define IMM_WIDTH               5
 `define FUNCT3_WIDTH            3
-`define VD_WIDTH                5
-`define RD_WIDTH                5
 `define OPCODE_WIDTH            7
 
 // Uops Queue data width
