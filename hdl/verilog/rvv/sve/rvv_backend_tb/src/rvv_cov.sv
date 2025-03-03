8765f59364775839b2544f6b0c68ea86fc321695
pu.wang <pu.wang@verisilicon.com>
2025-03-03 21:20:47 +0800

    Fix typo
    Change-Id: I0239df8f78c98887e971a21011d3237283f48b78

--- hdl/verilog/rvv/sve/rvv_backend_tb/src/rvv_cov.sv
+++ hdl/verilog/rvv/sve/rvv_backend_tb/src/rvv_cov.sv
@@ -62,16 +62,16 @@ class rvv_cov extends uvm_component;
       coverpoint rx_tr.alu_inst;
     alu_type:
       coverpoint rx_tr.alu_type {
-        bins OPIVV = rvv_tb_pkg::OPIVV;
-        bins OPIVI = rvv_tb_pkg::OPIVI;
-        bins OPIVX = rvv_tb_pkg::OPIVX;
+        bins OPIVV = {rvv_tb_pkg::OPIVV};
+        bins OPIVI = {rvv_tb_pkg::OPIVI};
+        bins OPIVX = {rvv_tb_pkg::OPIVX};
 
-        bins OPMVV = rvv_tb_pkg::OPMVV;
-        bins OPMVX = rvv_tb_pkg::OPMVX;
+        bins OPMVV = {rvv_tb_pkg::OPMVV};
+        bins OPMVX = {rvv_tb_pkg::OPMVX};
         
-        illegal_bins OPFVV = rvv_tb_pkg::OPFVV;
-        illegal_bins OPFVF = rvv_tb_pkg::OPFVF;
-        illegal_bins OPCFG = rvv_tb_pkg::OPCFG;
+        illegal_bins OPFVV = {rvv_tb_pkg::OPFVV};
+        illegal_bins OPFVF = {rvv_tb_pkg::OPFVF};
+        illegal_bins OPCFG = {rvv_tb_pkg::OPCFG};
 
         illegal_bins misc = default;
       }
@@ -79,20 +79,20 @@ class rvv_cov extends uvm_component;
       coverpoint rx_tr.vm;
     vlmul:
       coverpoint rx_tr.vtype.vlmul {
-        bins LMUL1_4 = rvv_tb_pkg::LMUL1_4;
-        bins LMUL1_2 = rvv_tb_pkg::LMUL1_2;
-        bins LMUL1   = rvv_tb_pkg::LMUL1  ;
-        bins LMUL2   = rvv_tb_pkg::LMUL2  ;
-        bins LMUL4   = rvv_tb_pkg::LMUL4  ;
-        bins LMUL8   = rvv_tb_pkg::LMUL8  ;
+        bins LMUL1_4 = {rvv_tb_pkg::LMUL1_4};
+        bins LMUL1_2 = {rvv_tb_pkg::LMUL1_2};
+        bins LMUL1   = {rvv_tb_pkg::LMUL1  };
+        bins LMUL2   = {rvv_tb_pkg::LMUL2  };
+        bins LMUL4   = {rvv_tb_pkg::LMUL4  };
+        bins LMUL8   = {rvv_tb_pkg::LMUL8  };
 
         illegal_bins misc = default;
       }
     vsew:
       coverpoint rx_tr.vtype.vsew {
-        bins SEW8  = rvv_tb_pkg::SEW8 ; 
-        bins SEW16 = rvv_tb_pkg::SEW16;
-        bins SEW32 = rvv_tb_pkg::SEW32;
+        bins SEW8  = {rvv_tb_pkg::SEW8 }; 
+        bins SEW16 = {rvv_tb_pkg::SEW16};
+        bins SEW32 = {rvv_tb_pkg::SEW32};
 
         illegal_bins misc = default;
       }
@@ -108,10 +108,10 @@ class rvv_cov extends uvm_component;
       }
     vxrm:
       coverpoint rx_tr.vxrm {
-        bins RNU = rvv_tb_pkg::RNU;
-        bins RNE = rvv_tb_pkg::RNE;
-        bins RDN = rvv_tb_pkg::RDN;
-        bins ROD = rvv_tb_pkg::ROD;
+        bins RNU = {rvv_tb_pkg::RNU};
+        bins RNE = {rvv_tb_pkg::RNE};
+        bins RDN = {rvv_tb_pkg::RDN};
+        bins ROD = {rvv_tb_pkg::ROD};
         
         illegal_bins misc = default;
       }
