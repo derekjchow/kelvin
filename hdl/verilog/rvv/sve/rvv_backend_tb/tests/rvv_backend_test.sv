a1f15c3d0d8b6d417c41cfa93319e6fa66a3e1dc
pu.wang <pu.wang@verisilicon.com>
2025-03-04 09:54:49 +0800

    Fix tb for execution time
    Change-Id: I2f32087685ab40c5457b29606b2c97aeb6ea631c

--- hdl/verilog/rvv/sve/rvv_backend_tb/tests/rvv_backend_test.sv
+++ hdl/verilog/rvv/sve/rvv_backend_tb/tests/rvv_backend_test.sv
@@ -2052,7 +2052,7 @@ class alu_gather_test extends rvv_backend_test;
 
     rvs_last_seq = alu_smoke_vv_seq::type_id::create("rvs_last_seq", this);
     rvs_last_seq.run_inst(VRGATHER,env.rvs_agt.rvs_sqr);
-    phase.phase_done.set_drain_time(this, 2000ns);
+    phase.phase_done.set_drain_time(this, 5000ns);
     phase.drop_objection( .obj( this ) );
   endtask
 
