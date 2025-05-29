27cb6f5f6d91616ca0729c18c922ab7631e12439
Alex Van Damme <atv@google.com>
2025-05-29 15:24:49 -0700

    Chisel BlackBox around SV Aligner module
    Change-Id: I7b2877fa48ed7a667de442a2a7192643dd0e8297

---
+++ hdl/chisel/src/common/Aligner.scala
@@ -0,0 +1,84 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package common
+
+import chisel3._
+import chisel3.util._
+
+object GenerateAlignerSource {
+    def apply[T <: Data](t: T, n: Int): String = {
+        var moduleInterface =  "module Aligner_T_WIDTH_GENN(\n".replaceAll("T_WIDTH", t.getWidth.toString)
+                                                                 .replaceAll("GENN", n.toString)
+        for (i <- 0 until n) {
+            moduleInterface += "  input logic in_GENI_valid,\n".replaceAll("GENI", i.toString)
+        }
+        for (i <- 0 until n) {
+            moduleInterface += "  input logic [T_WIDTH-1:0] in_GENI_bits,\n".replaceAll("GENI", i.toString)
+                                                                            .replaceAll("T_WIDTH", t.getWidth.toString)
+        }
+        for (i <- 0 until n) {
+            moduleInterface += "  output logic out_GENI_valid,\n".replaceAll("GENI", i.toString)
+        }
+        for (i <- 0 until n) {
+            moduleInterface += "  output logic [T_WIDTH-1:0] out_GENI_bits,\n".replaceAll("GENI", i.toString)
+                                                                              .replaceAll("T_WIDTH", t.getWidth.toString)
+        }
+        moduleInterface = moduleInterface.dropRight(2)
+        moduleInterface += ");\n\n"
+
+        var coreInstantiation = "  logic [GENN-1:0] valid_in;\n".replaceAll("GENN", n.toString)
+        for (i <- 0 until n) {
+            coreInstantiation += "  assign valid_in[GENI] = in_GENI_valid;\n".replaceAll("GENI", i.toString)
+        }
+        coreInstantiation += "  logic [GENN-1:0][T_WIDTH-1:0] data_in;\n".replaceAll("GENN", n.toString)
+                                                                         .replaceAll("T_WIDTH", t.getWidth.toString)
+        for (i <- 0 until n) {
+            coreInstantiation += "  assign data_in[GENI] = in_GENI_bits;\n".replaceAll("GENI", i.toString)
+        }
+        coreInstantiation += "  logic [GENN-1:0] valid_out;\n".replaceAll("GENN", n.toString)
+        for (i <- 0 until n) {
+            coreInstantiation += "  assign out_GENI_valid = valid_out[GENI];\n".replaceAll("GENI", i.toString)
+        }
+        coreInstantiation += "  logic [GENN-1:0][T_WIDTH-1:0] data_out;\n".replaceAll("GENN", n.toString)
+                                                                         .replaceAll("T_WIDTH", t.getWidth.toString)
+        for (i <- 0 until n) {
+            coreInstantiation += "  assign out_GENI_bits = data_out[GENI];\n".replaceAll("GENI", i.toString)
+        }
+        coreInstantiation += """
+        |  Aligner#(.T (logic [T_WIDTH-1:0]), .N(GENN)) aligner(
+        |    valid_in,
+        |    data_in,
+        |    valid_out,
+        |    data_out
+        |  );
+        |""".replaceAll("T_WIDTH", t.getWidth.toString)
+            .replaceAll("GENN", n.toString)
+            .stripMargin
+
+        moduleInterface + coreInstantiation + "endmodule\n"
+    }
+}
+
+class Aligner[T <: Data](t: T, n: Int) extends BlackBox with HasBlackBoxInline
+                                 with HasBlackBoxResource {
+    override val desiredName = "Aligner_T_WIDTH_GENN".replaceAll("T_WIDTH", t.getWidth.toString)
+                                                       .replaceAll("GENN", n.toString)
+    val io = IO(new Bundle {
+        val in = Input(Vec(n, Valid(UInt(t.getWidth.W))))
+        val out = Output(Vec(n, Valid(UInt(t.getWidth.W))))
+    })
+    addResource("hdl/verilog/rvv/design/Aligner.sv")
+    setInline(s"$desiredName.sv", GenerateAlignerSource(t, n))
+}
---
+++ hdl/chisel/src/common/AlignerTest.scala
@@ -0,0 +1,122 @@
+// Copyright 2025 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package common
+
+import chisel3._
+import chisel3.util._
+import chiseltest._
+import org.scalatest.freespec.AnyFreeSpec
+import scala.util.Random
+
+class AlignerTester[T <: Data](t: T, n: Int) extends Module {
+    val io = IO(new Bundle {
+        val in = Input(Vec(n, Valid(t)))
+        val out = Output(Vec(n, Valid(t)))
+    })
+    val aligner = Module(new Aligner(t, n))
+    for (i <- 0 until n) {
+        aligner.io.in(i).valid := io.in(i).valid
+        aligner.io.in(i).bits := io.in(i).bits.asUInt
+
+        io.out(i).valid := aligner.io.out(i).valid
+        io.out(i).bits := aligner.io.out(i).bits.asTypeOf(t)
+    }
+}
+
+class AlignerSpec extends AnyFreeSpec with ChiselScalatestTester {
+    "Basic" in {
+        val n = 4
+        test (new AlignerTester(UInt(32.W), n))
+        .withAnnotations(
+            Seq(
+                VerilatorBackendAnnotation,
+            )
+        ) { dut =>
+            for (i <- 0 until 1000) {
+                var valid_in_count = 0
+                for (i <- 0 until n) {
+                    val valid_in = Random.between(0, 2)
+                    dut.io.in(i).valid.poke(valid_in)
+                    valid_in_count += valid_in
+
+                    val data_in = Random.between(0, Math.pow(2, 32)).toInt
+                    dut.io.in(i).bits.poke(data_in)
+                }
+
+                var valid_out_count = 0
+                for (i <- 0 until n) {
+                    valid_out_count += dut.io.out(i).valid.peekInt().toInt
+                }
+                assertResult(true) { valid_in_count == valid_out_count }
+
+                var outIdx = 0
+                for (i <- 0 until n) {
+                    val valid_in = dut.io.in(i).valid.peekInt()
+                    val data_in = dut.io.in(i).bits.peekInt()
+                    val data_out = dut.io.out(outIdx).bits.peekInt()
+                    if (valid_in == 1) {
+                        assertResult(true) { data_in == data_out }
+                        outIdx = outIdx + 1
+                    }
+                }
+            }
+        }
+    }
+
+    "Bundle" in {
+        val n = 4
+        class bundleT extends Bundle {
+            val a = UInt(32.W)
+            val b = UInt(5.W)
+            val c = UInt(12.W)
+        }
+        test (new AlignerTester(new bundleT, n))
+        .withAnnotations(
+            Seq(
+                VerilatorBackendAnnotation,
+            )
+        ) { dut =>
+            for (i <- 0 until 1000) {
+                var valid_in_count = 0
+                for (i <- 0 until n) {
+                    val valid_in = Random.between(0, 2)
+                    dut.io.in(i).valid.poke(valid_in)
+                    valid_in_count += valid_in
+
+                    dut.io.in(i).bits.a.poke(Random.between(0, Math.pow(2, dut.io.in(i).bits.a.getWidth)).toInt)
+                    dut.io.in(i).bits.b.poke(Random.between(0, Math.pow(2, dut.io.in(i).bits.b.getWidth)).toInt)
+                    dut.io.in(i).bits.c.poke(Random.between(0, Math.pow(2, dut.io.in(i).bits.c.getWidth)).toInt)
+                }
+
+                var valid_out_count = 0
+                for (i <- 0 until n) {
+                    valid_out_count += dut.io.out(i).valid.peekInt().toInt
+                }
+                assertResult(true) { valid_in_count == valid_out_count }
+
+                var outIdx = 0
+                for (i <- 0 until n) {
+                    val valid_in = dut.io.in(i).valid.peekInt()
+                    if (valid_in == 1) {
+                        assertResult(dut.io.in(i).bits.a.peekInt()) { dut.io.out(outIdx).bits.a.peekInt() }
+                        assertResult(dut.io.in(i).bits.b.peekInt()) { dut.io.out(outIdx).bits.b.peekInt() }
+                        assertResult(dut.io.in(i).bits.c.peekInt()) { dut.io.out(outIdx).bits.c.peekInt() }
+                        outIdx = outIdx + 1
+                    }
+                }
+            }
+        }
+    }
+}
