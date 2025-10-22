21f78ed812511ffa19700dd816adbf736853f737
Alex Van Damme <atv@google.com>
2025-10-22 11:05:08 -0700

    feat(fpga): Add high-memory FPGA configuration
    This change introduces a parallel high-memory configuration for the FPGA build, allowing the SoC to be synthesized with a larger memory map.
    
    Key changes:
    - Created separate build targets for default and high-memory Verilator and Vivado builds.
    - Introduced a `CHISEL_SUBSYSTEM_HIGHMEM` CFLAG to differentiate between the two configurations in the Verilator testbench.
    - Reorganized the Chisel subsystem IP directory to support both default and high-memory variants.
    - Made the Chisel subsystem IP cores "virtual" to allow the build system to select the appropriate one at build time.
    
    Change-Id: I45244caa8cf804ecc5d77d609b47c2be48fc7880

--- fpga/ip/coralnpu_chisel_subsystem_highmem/coralnpu_chisel_subsystem_highmem.core.tpl
+++ fpga/ip/coralnpu_chisel_subsystem_highmem/coralnpu_chisel_subsystem_highmem.core.tpl
@@ -5,16 +5,18 @@ CAPI=2:
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#      http://www.apache.org/licenses/LICENSE-2.0
 #
-# Unless required by applicable law of an "AS IS" BASIS,
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-name: "coralnpuv2:ip:coralnpu_chisel_subsystem:0.1"
+name: "coralnpuv2:ip:coralnpu_chisel_subsystem_highmem:0.1"
 description: "CoralNPU SoC Unified Chisel Subsystem"
-
+virtual:
+  - coralnpuv2:virtual:coralnpu_chisel_subsystem
 filesets:
   rtl:
     depend:
--- fpga/main.cc
+++ fpga/main.cc
@@ -18,6 +18,12 @@
 #include "verilator_memutil.h"
 #include "verilator_sim_ctrl.h"
 
+#if defined(CHISEL_SUBSYSTEM_HIGHMEM)
+constexpr bool highmem = true;
+#else
+constexpr bool highmem = false;
+#endif
+
 int main(int argc, char **argv) {
   chip_verilator top;
   VerilatorMemUtil memutil;
@@ -25,6 +31,9 @@ int main(int argc, char **argv) {
   simctrl.SetTop(&top, &top.clk_i, &top.rst_ni,
                  VerilatorSimCtrlFlags::ResetPolarityNegative);
 
+  constexpr uint32_t itcm_size = highmem ? 0x100000 : 0x2000;
+  constexpr uint32_t dtcm_size = highmem ? 0x100000 : 0x8000;
+  constexpr uint32_t dtcm_addr = highmem ? 0x100000 : 0x10000;
   // NB: Final parameter here is "width" of your memory, penultimate parameter
   // is "depth".
   MemArea rom("TOP.chip_verilator.i_coralnpu_soc.i_rom.u_prim_rom", 0x8000 / 4,
@@ -33,16 +42,16 @@ int main(int argc, char **argv) {
   MemArea itcm(
       "TOP.chip_verilator.i_coralnpu_soc.i_coralnpu_core.coreAxi.itcm.sram."
       "sramModules_0",
-      0x2000 / 16, 16);
+      itcm_size / 16, 16);
   MemArea dtcm(
       "TOP.chip_verilator.i_coralnpu_soc.i_coralnpu_core.coreAxi.dtcm.sram."
       "sramModules_0",
-      0x8000 / 16, 16);
+      dtcm_size / 16, 16);
 
   memutil.RegisterMemoryArea("rom", 0x10000000, &rom);
   memutil.RegisterMemoryArea("sram", 0x20000000, &sram);
   memutil.RegisterMemoryArea("itcm", 0x00000000, &itcm);
-  memutil.RegisterMemoryArea("dtcm", 0x00010000, &dtcm);
+  memutil.RegisterMemoryArea("dtcm", dtcm_addr, &dtcm);
   simctrl.RegisterExtension(&memutil);
 
   simctrl.SetInitialResetDelay(2000);
